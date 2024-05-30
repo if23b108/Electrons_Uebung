65da420681efe528037920aad1f270293247b373
Leon <leonfriedl1530@gmail.com>
2024-05-30 19:52:39 +0200

    Dritter und Vierter Testcase

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -10,7 +10,11 @@ function electronsAroundCores (array) {
 	if(length==4){
 		ergebnis = (array[0]+array[1]);
 	}
-	
+	if(length==6){
+		ergebnis = (Math.pow(array[0]-array[1],2)+ Math.pow(array[2]-array[3],2)+ Math.pow(array[4]-array[5],2));
+		if(ergebnis<10){
+			ergtebnis=ergebnis%2;		}
+	}
 
 	return ergebnis;
 }
